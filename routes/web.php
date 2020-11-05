26f0cd1428e1b61a143b4b94778d7251683212b6
Anwesha Mohanty <anwesha0322@gmail.com>
2020-11-05 20:05:23 +0530

    added login form,changed homepage

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UsersController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+
+Route::get('/login',function()
+{
+    return view('login');
+});
+Route::post('/login','UsersController@store');
